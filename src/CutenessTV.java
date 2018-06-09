adf458412df48530cafbffdf1547bd489e8b5073
EmperorlNeo <league@WorthyMini.local>
2018-06-09 11:28:24 -0700

    CutenessTV - finished, SoundEffects - unfinished
    Day 6

--- src/CutenessTV.java
+++ src/CutenessTV.java
@@ -7,52 +7,68 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class CutenessTV implements ActionListener {
-	public static void main(String [] args) {
+	public static void main(String[] args) {
 		CutenessTV cute = new CutenessTV();
-		cute.showButton();		
+		cute.showButton();
 	}
+
 	private void showButton() {
 		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		frame.setSize(500, 500);
 		JPanel panel = new JPanel();
 		frame.add(panel);
 		JButton button1 = new JButton("Ducks");
+		button1.setAlignmentX(250);
 		panel.add(button1);
 		frame.add(panel);
 		button1.addActionListener(this);
 		JButton button2 = new JButton("Frog");
+		button2.setAlignmentX(100);
 		panel.add(button2);
 		frame.add(panel);
 		button2.addActionListener(this);
 		JButton button3 = new JButton("Fluffy Unicorn");
+		button3.setAlignmentX(400);
 		panel.add(button3);
 		frame.add(panel);
 		button3.addActionListener(this);
+		frame.pack();
 	}
-//	void showDucks() {
-//	     playVideo("https://www.youtube.com/watch?v=MtN1YnoL46Q");
-//	}
-//
-//	void showFrog() {
-//	     playVideo("https://www.youtube.com/watch?v=cBkWhkAZ9ds");
-//	}
-//
-//	void showFluffyUnicorns() {
-//	     playVideo("https://www.youtube.com/watch?v=a-xWhG4UU_Y");
-//	}
-//
-//	void playVideo(String videoID) {
-//	     try {
-//	          URI uri = new URI(videoID);
-//	          java.awt.Desktop.getDesktop().browse(uri);
-//	     } catch (Exception e) {
-//	          e.printStackTrace();
-//	     }
-//	}
+
+	void showDucks() {
+		
+		playVideo("https://www.youtube.com/watch?v=MtN1YnoL46Q");
+	}
+
+	void showFrog() {
+		playVideo("https://www.youtube.com/watch?v=cBkWhkAZ9ds");
+	}
+
+	void showFluffyUnicorns() {
+		playVideo("https://www.youtube.com/watch?v=a-xWhG4UU_Y");
+	}
+
+	void playVideo(String videoID) {
+		try {
+			URI uri = new URI(videoID);
+			java.awt.Desktop.getDesktop().browse(uri);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
+		if (e.getSource().toString().contains("Ducks")) {
+			showDucks();
+		}
+		else if (e.getSource().toString().contains("Frog")) {
+			showFrog();
+		}
+		else if (e.getSource().toString().contains("Fluffy Unicorn")) {
+			showFluffyUnicorns();
+		}
 	}
 }
--- src/SoundEffects.java
+++ src/SoundEffects.java
@@ -1,15 +1,27 @@
 import java.applet.AudioClip;
 
 import javax.swing.JApplet;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class SoundEffects {
-
+	
 	public static void main(String[] args) {
 		SoundEffects se = new SoundEffects();
 		se.playSound("sawing-wood-daniel_simon.wav");
 	}
 	
-	
+	private void showButton() {
+		JFrame frame = new JFrame();
+		frame.setSize(500, 500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		JPanel panel = new JPanel();
+		frame.add(panel);
+		JButton button1 = new JButton("TARDIS");
+		
+	}
 	
 	
 	private void playSound(String fileName) {
